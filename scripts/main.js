d109a4a7bb12afd6a999b4defa5651a5f6eed062
Mike Theodorsson <mikthe@clio.me>
2021-01-13 11:59:08 +0100

    Localization

--- scripts/main.js
+++ scripts/main.js
@@ -6,24 +6,23 @@ class PlaylistControllerInitializer {
     static initialize() {
         PlaylistControllerInitializer.hookReady();
         PlaylistControllerInitializer.hookRenderPlaylistSearch();
-        PlaylistControllerInitializer.hookInit();
     }
 
     static hookRenderPlaylistSearch() {
         /**
-         * Appends a search bar onto the playlist screen for finding songs
+         * Appends a search bar onto the playlist screen for finding songs easily
          */
-
         Hooks.on("renderPlaylistDirectory", (app, html, data) => {
-            //const importPlaylistString = game.i18n.localize("PLI.ImportButton");
-            const searchString = "Search";
+            const searchString = game.i18n.localize("PLCR.Search");
             const searchField = $(`<input type="text" placeholder="${searchString}" style="color: white; max-width: 96%; margin: 10px 0px;" />`);
-            const searchResults = $(`<li id="plcr-search-results" data-entity-id="9m16PqGayex5CSIY" class="directory-item entity playlist plcr-sr flexrow"></li>`);
             html.find(".directory-header").append(searchField)
-            html.find(".directory-list").prepend(searchResults);
             searchField.keyup(debounce(function(ev) {
                 PLCR.playlistController.search(ev.target.value).then(function(result) {
-                    html.find("#plcr-search-results").html(result);
+                    if (typeof result != 'undefined') {
+                        console.log('PLCR | Found ' + result.length + ' results.');
+                    } else {
+                        console.log('PLCR | Found 0 results.');
+                    }
                 }, function(err) {
                     console.log(err);
                 });
@@ -37,51 +36,33 @@ class PlaylistControllerInitializer {
             // TO-DO: Register all settings
         });
     }
-
-    static hookInit() {
-        Hooks.once( "init", function() {
-            PlaylistControllerInitializer.loadHandleBarTemplates();
-        });
-    }
-
-    async loadHandleBarTemplates()
-    {
-        // register templates parts
-        const templatePaths = [
-            "./modules/playlist-ui/templates/search-results.html"
-        ];
-        return loadTemplates(templatePaths);
-    }
 }
 
 class PlaylistController {
     constructor() {
-        /*  --------------------------------------  */
-        /*            Global settings               */
-        /*  --------------------------------------  */
-        this.DEBUG = false; // Enable to see logs
+        this.directoryItemSelector = '#playlists .directory-item:not(.global-volume)';
+        this.playlistSelector = '#playlists .playlist:not(.collapsed):not(.global-volume)';
     }
-    /*  --------------------------------------  */
-    /*           Helper functions               */
-    /*  --------------------------------------  */
 
     async search(searchString) {
         if (searchString.length > 3) {
-            let collection = $(`#playlists .directory-item:not(.plcr-sr) .sound-name:contains(${searchString}):not(selector)`);
+            let collection = $(this.directoryItemSelector + ` .sound-name:contains(${searchString}):not(selector)`);
             if(collection.length > 0) {
-                let rows = "";
+                $(this.directoryItemSelector + ` .sound`).hide();
                 collection.each(function() {
-                    rows += $(this).parent()[0].outerHTML;
+                    $(this).parent().show();
+                    $(this).parents('.playlist-sounds').show();
+                    $(this).parents('.playlist').removeClass('collapsed');
                 });
 
-                const data = {
-                    resultRows: rows
-                }
-                return await renderTemplate("./modules/playlist-ui/templates/search-results.html", data);
+                return collection;
             }
+        } else {
+            $(this.directoryItemSelector + ` .sound`).show();
+            $(this.playlistSelector + ` .playlist-sounds`).hide();
+            $(this.playlistSelector).addClass('collapsed');
         }
     }
-
 }
 
 PlaylistControllerInitializer.initialize();
